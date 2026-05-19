71f1d1321b8a64e8b141aec578e2ab5bda326b20
Dev.Muhamad <soti03e250058@epu.edu.iq>
2026-05-19 04:06:12 +0300

    Update firebase.js

--- assect/java/firebase.js
+++ assect/java/firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASJbu_rNecyaHi9gqrE9PSOwyA7pvq92E",
+  authDomain: "kurd-account.firebaseapp.com",
+  projectId: "kurd-account",
+  storageBucket: "kurd-account.firebasestorage.app",
+  messagingSenderId: "325274297633",
+  appId: "1:325274297633:web:04f9b4b209d3d0f6a90a60"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
