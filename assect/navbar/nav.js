727c7850920b53cb5edf3cdac88a92f202b01131
Ahmad siamand <soti03e250048@epu.edu.iq>
2026-05-25 20:00:08 +0300

    Update nav.js

--- assect/navbar/nav.js
+++ assect/navbar/nav.js
@@ -1,6 +1,6 @@
 function loadNavbar() {
 const navbarHTML = `
-    <nav class="navbar" id="navbar" >
+    <nav class="navbar" id="navbar">
       <div class="nav-container">
         <div class="nav-links-wrap">
           <ul class="nav-links">
